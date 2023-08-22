5444dd07e4ee48354881e459e4d34fe17678a00d
Gidk-g <devhaxel@gmail.com>
2023-08-22 23:21:04 +0200

    Update AssetsPaths.hx

--- source/game/AssetsPaths.hx
+++ source/game/AssetsPaths.hx
@@ -4,6 +4,11 @@ class AssetsPaths
 {
 	public static function getPath(filePath:String):String
 	{
+		var file:String = modding.Mods.getPath(filePath);
+		if (sys.FileSystem.exists(file))
+		{
+			return file;
+		}
 		return "assets/" + filePath;
 	}
 }
