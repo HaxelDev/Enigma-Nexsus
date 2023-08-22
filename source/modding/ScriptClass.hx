ef0aff318e32d4287695d4a4f6519868fb9d2c22
Gidk-g <devhaxel@gmail.com>
2023-08-23 01:30:48 +0200

    Update ScriptClass.hx

--- source/modding/ScriptClass.hx
+++ source/modding/ScriptClass.hx
@@ -6,11 +6,12 @@ import hscript.InterpEx;
 class ScriptClass
 {
 	public var interp:InterpEx;
+	public var instance:Dynamic;
 
 	public function new(name:String, contents:String)
 	{
 		interp = new InterpEx();
-		var instance = interp.createScriptClassInstance(name);
+		instance = interp.createScriptClassInstance(name);
 		interp.addModule(Resource.getString(game.AssetsPaths.getPath("data/" + contents + ".hx")));
 		instance.create();
 	}
